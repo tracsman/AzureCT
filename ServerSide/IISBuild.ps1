8aac2964ba0c8af2f03de34b3cc5bd54fb0b2d44
Jon Ormond <tracsman@hotmail.com>
2016-01-04 17:12:15 -0800

    Update pull method for server files and update default home page in web.config.

--- ServerSide/IISBuild.ps1
+++ ServerSide/IISBuild.ps1
@@ -2,94 +2,50 @@
 # Must be run from an elevated PS prompt!
 
 # Turn On ICMPv4
+    Write-Host
     Write-Host "Creating ICMP Rule in Windows Firewall" -ForegroundColor Cyan
     New-NetFirewallRule -Name Allow_ICMPv4 -DisplayName "Allow ICMPv4" -Protocol ICMPv4 -Enabled True -Profile Any -Action Allow -ErrorAction SilentlyContinue
 
 # Install IIS
+    Write-Host
     Write-Host "Installing IIS and .Net 4.5, this can take some time, like 15+ minutes..." -ForegroundColor Cyan
     add-windowsfeature Web-Server, Web-WebServer, Web-Common-Http, Web-Default-Doc, Web-Dir-Browsing, Web-Http-Errors, Web-Static-Content, Web-Health, Web-Http-Logging, Web-Performance, Web-Stat-Compression, Web-Security, Web-Filtering, Web-App-Dev, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Net-Ext, Web-Net-Ext45, Web-Asp-Net45, Web-Mgmt-Tools, Web-Mgmt-Console
 
 # Create Web App Pages
-    Write-Host "Creating Web pages, XML, and Web.Config file" -ForegroundColor Cyan
-    $MainPage = '<%@ Page Language="vb" AutoEventWireup="false" %>
-    <%@ Import Namespace="System.IO" %>
-    <script language="vb" runat="server">
-        Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-            lblTime.Text = Now()
-        End Sub
-    </script>
+    # Create Arrays
+    $FileName = @()
+    
+    # Stuff Arrays with Github URL and local file path
+    $FileName += "DisplayPing.html"
+    $FileName += "DisplayPing.css"
+    $FileName += "DisplayPing.js"
+    $FileName += "jquery.js"
+    $FileName += "Web.config"
+    $FileName += "DiagJobDetail.xml"
+    $FileName += "DiagJobHeader.xml"
+    $FileName += "UploadHeader.aspx"
+    $FileName += "UploadDetail.aspx"
+    $FileName += "ClearHistory.aspx"
 
-    <!DOCTYPE html>
-    <html xmlns="http://www.w3.org/1999/xhtml">
-    <head runat="server">
-        <title>Hybrid Diagnostic Home Page</title>
-    </head>
-    <body style="font-family: Optima,Segoe,Segoe UI,Candara,Calibri,Arial,sans-serif;">
-      <form id="frmMain" runat="server">
-        <div>
-          <h1>Looks like you made it!</h1>
-          This is a page from the inside (a web server on a private network),<br />
-          and it is making its way to the outside! (If you are viewing this from the internet)<br />
-          <br />
-          The following sections show:
-          <ul style="margin-top: 0px;">
-            <li> Local Server Time - Shows if this page is or isnt cached anywhere</li>
-            <li> File Output - Shows that the web server is reaching AppVM01 on the backend subnet and successfully returning content</li>
-            <li> Image from the Internet - Doesnt really show anything, but it made me happy to see this when the app worked</li>
-          </ul>
-          <div style="border: 2px solid #8AC007; border-radius: 25px; padding: 20px; margin: 10px; width: 650px;">
-            <b>Local Web Server Time</b>: <asp:Label runat="server" ID="lblTime" /></div>
-          <div style="border: 2px solid #8AC007; border-radius: 25px; padding: 20px; margin: 10px; width: 650px;">
-            <b>File Output from AppVM01</b></div>
-          <div style="border: 2px solid #8AC007; border-radius: 25px; padding: 20px; margin: 10px; width: 650px;">
-            <b>Image File Linked from the Internet</b>:<br />
-            <br />
-            <img src="http://sd.keepcalm-o-matic.co.uk/i/keep-calm-you-made-it-7.png" alt="You made it!" width="150" length="175"/></div>
-        </div>
-      </form>
-    </body>
-    </html>'
-
-    $WebTestPage = '
-    <%@ Page Language="vb" AutoEventWireup="false" %>
-    <%@ Import Namespace="System.IO" %>
-    <% Dim tTime as Date = Now() %>
-
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title><%=tTime%></title>
-    </head>
-    <body>
-        1.0
-    </body>
-    </html>'
-
-    $WebConfig ='<?xml version="1.0" encoding="utf-8"?>
-    <configuration>
-      <system.web>
-        <compilation debug="true" strict="false" explicit="true" targetFramework="4.5" />
-        <httpRuntime targetFramework="4.5" />
-        <customErrors mode="Off"/>
-      </system.web>
-      <system.webServer>
-        <defaultDocument>
-          <files>
-            <add value="Home.aspx" />
-          </files>
-        </defaultDocument>
-      </system.webServer>
-    </configuration>'
-
-    $JobHeaderXML = '<?xml version="1.0"?><Jobs></Jobs>'
-    $JobDetailXML = '<?xml version="1.0"?><JobRecords></JobRecords>'
-    Write-Host "Writing files to disk" -ForegroundColor Cyan
-    $MainPage | Out-File -FilePath "C:\inetpub\wwwroot\Home.aspx" -Encoding ascii
-    $WebTestPage | Out-File -FilePath "C:\inetpub\wwwroot\WebTest.aspx" -Encoding ascii
-    $WebConfig | Out-File -FilePath "C:\inetpub\wwwroot\Web.config" -Encoding ascii
-    $JobHeaderXML | Out-File -FilePath "C:\inetpub\wwwroot\JobHeader.xml" -Encoding ascii
-    $JobDetailXML | Out-File -FilePath "C:\inetpub\wwwroot\JobDetail.xml" -Encoding ascii
+    # Loop through the arrays and pull files from GitHub to the Server
+    Write-Host
+    Write-Host "Pulling Web pages from GitHub" -ForegroundColor Cyan
+    Write-Host "Coping files from the 'ServerSide' directory on GitHub to the local 'c:\inetpub\wwwroot\' directory."
+    $Issues = $false
+    $i=0
+    $URL | ForEach{
+        Try {
+            $webClient = new-object System.Net.WebClient
+            $webClient.DownloadFile( "https://raw.githubusercontent.com/tracsman/HybridTool/master/ServerSide/" + $FileName[$i], "c:\inetpub\wwwroot\" + $FileName[$i] )
+            Write-Host "Copied successfully:" $FileName[$i] 
+        }
+        Catch {
+            $Issues = $true
+            Write-Host "Download Error:" $FileName[$i] "was not downloaded. Copy this file manually, or rerun this script." -ForegroundColor Red}
+        $i++
+    }
 
     Write-Host
-    Write-Host "Web App Creation Successfull!" -ForegroundColor Green
+    If ($Issues) {Write-Host "Issues encountered, please see statement above in red for details!" -ForegroundColor Red}
+    Else {Write-Host "Web App Creation Successfull!" -ForegroundColor Green}
     Write-Host
