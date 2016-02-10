0b569a9d954a3188da20a882d7739d71cedadaba
Jon Ormond <tracsman@hotmail.com>
2016-02-09 16:48:37 -0800

    Improve Get-IPTrace

--- PowerShell/AzureCT/Public/LocalTrace.ps1
+++ PowerShell/AzureCT/Public/LocalTrace.ps1
@@ -1,4 +1,4 @@
-﻿function LocalTrace {
+﻿function Get-IPTrace {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline=$true, HelpMessage='Enter IP Address of Remote Azure VM')]
@@ -6,24 +6,64 @@
 
     $Ping = New-Object -TypeName System.Net.NetworkInformation.Ping
     $PingOptions = New-Object -TypeName System.Net.NetworkInformation.PingOptions
-    $Payload = [byte[]][char[]]'MyData1'
+    $Payload = [byte[]][char[]]'MyData'
     $i = 1
 
-    while ($i -lt 30) {
+    while ($i -le 30) {
         $PingOptions.Ttl = $i
-        $Response = $Ping.Send($RemoteHost, 1000, $Payload, $PingOptions)
+        $Response = $Ping.Send($RemoteHost, 1500, $Payload, $PingOptions)
         $Trace = New-Object -TypeName PSObject -Property @{
             TTL           = $PingOptions.Ttl;
-            Status        = $Response.Status;
             Address       = $Response.Address;
             RoundTripTime = $Response.RoundtripTime;
+            MachineName   = ''
             }
-        If ($Trace.Status -eq 'Success') {break}
+        $TraceStatus = $Response.Status
+        If ($TraceStatus -eq 'Success') {
+            $Trace.MachineName   = Get-HostName $Trace.Address
+            Write-Output -InputObject $Trace
+            Break
+        }
+        ElseIf ($TraceStatus -eq 'TtlExpired') {
+            $PingOptions.Ttl = $i + 2
+            $Response = $Ping.Send($Trace.Address, 1500, $Payload, $PingOptions)
+            If ($Response.Status -eq 'TimedOut') {
+                $Trace.MachineName   = Get-HostName $Trace.Address
+                $Trace.RoundTripTime = '*'
+            }
+            Else {
+                $Trace.RoundTripTime = $Response.RoundtripTime
+                $Trace.MachineName   = Get-HostName $Trace.Address
+            }
+        }
+        ElseIf ($TraceStatus -eq 'TimedOut') {
+            #$Trace.MachineName   = Get-HostName $Trace.Address
+            $Trace.Address = 'Hop Timeout'
+            $Trace.RoundTripTime = '*'
+        }
         Write-Output -InputObject $Trace
         
         $i++
     } # End While
-    $Trace
+
 }
 
-LocalTrace -RemoteHost 10.249.172.52
+function Get-HostName {
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline=$true, HelpMessage='Enter IP Address')]
+        [ipaddress] $HostIP)
+    Try {
+        Return [System.Net.Dns]::GetHostEntry($HostIP).HostName
+    }
+    Catch {
+        Return ''
+    }
+}
+
+Get-IPTrace -RemoteHost 10.249.172.52
+
+# $Ping = New-Object -TypeName System.Net.NetworkInformation.Ping
+# $PingOptions = New-Object -TypeName System.Net.NetworkInformation.PingOptions
+# $PingOptions.Ttl = 12
+# $Payload = [byte[]][char[]]'MyData1'
+# $foo = $Ping.Send('10.249.172.52', 1000, $Payload, $PingOptions)
